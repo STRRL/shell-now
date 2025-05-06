a804147a0fe3e93c991315f939a2514ba445b69b
STRRL <im@strrl.dev>
2025-05-05 22:24:49 -0700

    chore: prepare cloudflared and ttyd
    Signed-off-by: STRRL <im@strrl.dev>

--- Dockerfile
+++ Dockerfile
@@ -12,6 +12,15 @@ RUN go build -o shell-now ./cmd/shell-now
 # Run stage
 FROM nicolaka/netshoot:v0.13
 
+# download cloudflared
+RUN curl -L https://github.com/cloudflare/cloudflared/releases/download/2025.4.2/cloudflared-linux-amd64 -o /usr/local/bin/cloudflared
+RUN chmod +x /usr/local/bin/cloudflared
+
+# download ttyd
+RUN curl -L https://github.com/tsl0922/ttyd/releases/download/1.7.7/ttyd.x86_64 -o /usr/local/bin/ttyd
+RUN chmod +x /usr/local/bin/ttyd
+
+# copy shell-now binary
 COPY --from=builder /app/shell-now /usr/local/bin/shell-now
 
 ENTRYPOINT ["shell-now"]
--- pkg/ttyd.go
+++ pkg/ttyd.go
@@ -13,11 +13,27 @@ func startTtyd(ctx context.Context,
 	credential string,
 ) error {
 	slog.Info("starting ttyd", "port", listenPort, "credential", credential)
+	startupCommand, err := fetchAvailableStartupCommand(ctx)
+	if err != nil {
+		return fmt.Errorf("fetch available startup command: %w", err)
+	}
 	// execute ttyd <options> sh
-	cmd := exec.CommandContext(ctx, "ttyd", "--writable", "--port", fmt.Sprintf("%d", listenPort), "--credential", credential, "sh")
+	cmd := exec.CommandContext(ctx, "ttyd", "--writable", "--port", fmt.Sprintf("%d", listenPort), "--credential", credential, startupCommand)
 
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
 	return cmd.Run()
+}
+
+func fetchAvailableStartupCommand(ctx context.Context) (string, error) {
+	// test commands in PATH,
+	// zsh, fish, bash, sh, login
+	commands := []string{"zsh", "fish", "bash", "sh", "login"}
+	for _, command := range commands {
+		if _, err := exec.LookPath(command); err == nil {
+			return command, nil
+		}
+	}
+	return "", fmt.Errorf("no available startup command found, auto detect failed with zsh, fish, bash, sh, login")
 }
